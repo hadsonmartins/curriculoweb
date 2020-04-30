fec8bf329e727c8135efb359c8041e71526ec05d
hadsondev <hadsonmartins10@gmail.com>
2020-04-30 16:17:13 -0400

    semana1

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/styles.css'
+
+
+const App = ({Component, pageProps }) => {
+    return (
+        <div> 
+        <Component {...pageProps}/>
+
+        </div>
+     )
+}
+export default App
---
+++ pages/index.js
@@ -0,0 +1,78 @@
+import React from 'react'
+
+const Index = () => {
+    return (
+        <div>
+       <div className='mt-3 mx-20 p-1 text-center text-2xl font-bold'>
+        Hadson Martins de Moraes
+        </div>
+
+        <div className='ml-9 text-black text-center'>
+        24 Anos<br/>
+        Cristo rei, Várzea Grande - Mato Grosso<br/>
+        hadson_martins@hotmail.com <br/>
+        (65) 992713476<br/>
+        </div>
+
+        <div className="mt-4 mx-20 p-1 bg-gray-400 text-center font-bold">
+        Objetivo
+        </div>
+        <div className='ml-20 text-black'>
+        Trabalhar em Desenvolvimento Fullstack
+        </div>
+
+        <div className="mt-8 mx-20 p-1 bg-gray-400 text-center font-bold">
+        Formação Acadêmica
+        </div>
+        <div className='ml-20 text-black'>
+        Cursando Sistemas de Informação <br/>
+          Centro Universitário de Várzea Grande - UNIVAG <br/>
+          2015 - Atual<br/>
+          <br/>
+          2º grau Completo - Curso Técnico de Tecnologia da Informação<br/>
+          Escola Estadual Julio Strubing Muller, Varzea Grande - MT <br/>
+          2011 - 2014<br/>
+        </div>
+
+        <div className="mt-3 mx-20 p-1 bg-gray-400 text-center font-bold">
+        Formação Complementar
+        </div>
+        <div className='ml-20 text-black'>
+        Fullstack Master - DevPleno <br/>
+        <a href='https://lp.devpleno.com/fullstack-master-listadeespera/?ref=I19258166P' target='_blank'>https://lp.devpleno.com/</a><br/>
+          2020 - Atual<br/>
+          <br/>
+          Inglês Extremo - Curso Completo do Zero à Fluência<br/>
+          <a href='https://www.udemy.com/' target='_blank'>https://www.udemy.com/</a><br/>
+           2019 - Atual
+          </div>
+
+          <div className="mt-4 mx-20 p-1 bg-gray-400 text-center font-bold">
+          Experiência Profissional
+          </div>
+          <div className='ml-20 text-black'>
+          Estagiário em Ti<br/>
+          Centro Universitário de Várzea Grande - UNIVAG<br/>
+          2015 - 2017<br/>
+          Função: Auxiliar na Infraestrutura em redes, manutenção preventiva e suporte aos usuários internos da empresa<br/>
+          <br/>
+          Aprendiz surporte de Ti<br/>
+          Centro Universitário de Várzea Grande - UNIVAG<br/>
+          Temporário Agosto 2013 - outubro 2013<br/>
+          Função: Auxiliar no Suporte de laboratorio da faculdade<br/>
+          </div>
+          
+          <div className="mt-4 mx-20 p-1 bg-gray-400 text-center font-bold">
+          Idiomas
+          </div>
+          <div className='ml-20 text-black'>
+          Português<br/>
+          Inglês Básico <br/>
+          <br/>
+          </div>
+
+        </div>
+        
+   )
+}
+export default Index
